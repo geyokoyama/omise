f670250542ba388ad2157cb28b3309ee21dda662
geyokoyama <george_y@outlook.com>
2022-09-24 13:22:57 -0700

    Update syntax

--- config/initializers/generators.rb
+++ config/initializers/generators.rb
@@ -1,9 +1,9 @@
 Rails.application.config.generators do |g|
-  g.test_framework :rspec,
-    fixtures:         false,
-    view_specs:       false,
-    helper_specs:     false,
-    routing_specs:    false,
-    request_specs:    false,
-    controller_specs: false
+  g.test_framework :rspec
+  g.fixtures         false
+  g.view_specs       false
+  g.helper_specs     false
+  g.routing_specs    false
+  g.request_specs    false
+  g.controller_specs false
 end
