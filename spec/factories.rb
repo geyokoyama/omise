c2ec7aab1c05f7ed20ac4056f675a024bce628b1
geyokoyama <george_y@outlook.com>
2022-09-24 19:47:39 -0700

    Generate Product scaffold

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,2 +1,9 @@
 FactoryBot.define do
+  factory :product do
+    title { Faker::Commerce.product_name }
+    price { Faker::Commerce.price(range: 0..1000.0) }
+    description { Faker::Lorem.paragraph }
+    brand { Faker::Commerce.brand }
+  end
+
 end
