25a764ba2ea91175cfeddf75bc9ebe431f547cb8
geyokoyama <george_y@outlook.com>
2022-09-27 11:53:20 -0700

    Add validatations to Product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,8 @@
 class Product < ApplicationRecord
+  validates :title, presence: true,
+    length: { minimum: 1, maximum: 100 }
+  validates :price, presence: true,
+    numericality: { greater_than_or_equal_to: 0 }
+  validates :description, length: { maximum: 501 }
+  validates :brand, presence: true
 end
