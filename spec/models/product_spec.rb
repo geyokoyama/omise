0ea9e13d3c00dc4e35f5404946cd550cab30835b
geyokoyama <george_y@outlook.com>
2022-09-27 11:52:53 -0700

    Fix specs

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -3,53 +3,47 @@ require 'rails_helper'
 RSpec.describe Product, type: :model do
   it "creates a new record with valid attributes" do
     expect(Product.count).to eq(0)
-    @product = create(:product)
+    create(:product)
     expect(Product.count).to eq(1)
   end
 
   describe "title" do
     it "must be present" do
-      expect(Product.count).to eq(0)
-      @product = create(:product, title: "")
-      expect(Product.count).to eq(0)
+      product = build(:product, title: nil)
+      expect { product.save! }.to raise_exception(ActiveRecord::RecordInvalid)
     end
 
     it "must be between 1 and 100 characters" do
-      expect(Product.count).to eq(0)
-      @product = create(:product, title: "")
-      @product = create(:product, title: "#" * 101)
-      expect(Product.count).to eq(0)
+      product = build(:product, title: "")
+      expect { product.save! }.to raise_exception(ActiveRecord::RecordInvalid)
+      product = build(:product, title: "#" * 101)
+      expect { product.save! }.to raise_exception(ActiveRecord::RecordInvalid)
     end
   end
 
   describe "price" do
     it "must be present" do
-      expect(Product.count).to eq(0)
-      @product = create(:product, price: nil)
-      expect(Product.count).to eq(0)
+      product = build(:product, price: nil)
+      expect { product.save! }.to raise_exception(ActiveRecord::RecordInvalid)
     end
 
     it "must be a number" do
-      expect(Product.count).to eq(0)
-      @product = create(:product, price: -1)
-      @product = create(:product, price: 5001)
-      expect(Product.count).to eq(0)
+      product = build(:product, price: -1)
+      expect { product.save! }.to raise_exception(ActiveRecord::RecordInvalid)
     end
   end
 
   describe "description do" do
     it "must be less than 500 words" do
-      expect(Product.count).to eq(0)
-      @product = create(:product, description: "#" * 501)
-      expect(Product.count).to eq(0)
+      product = build(:product, description: "# " * 501)
+      expect { product.save! }.to raise_exception(ActiveRecord::RecordInvalid)
     end
   end
 
   describe "brand" do
     it "must be present" do
-      expect(Product.count).to eq(0)
-      @product = create(:product, brand: "")
-      expect(Product.count).to eq(0)
+      product = build(:product, brand: "")
+      expect { product.save! }.to raise_exception(ActiveRecord::RecordInvalid)
     end
   end
 end
